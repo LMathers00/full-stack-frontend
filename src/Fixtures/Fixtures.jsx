c4e1c279478896f5439e7275bdac083bce6727e8
LMathers00 <lukemathers@hotmail.co.uk>
2022-09-20 14:06:43 +0100

    fixtures now displays as table

--- src/Fixtures/Fixtures.jsx
+++ src/Fixtures/Fixtures.jsx
@@ -21,20 +21,41 @@ const Fixtures = () => {
     
   
     return (
-      <ul>
-        {match.map((game) => {
-          console.log(game)
+      // <ul>
+      //   {match.map((game) => {
+      //     console.log(game)
           
-          return(
+      //     return(
           
           
-          <li key={game.match_id} > 
-            <p>Match No: {game.match_id }:</p>
-            <p> homeTeamID = {game.homeTeamID} homeTeam Score = {game.homeTeam_Score}</p>
-            <p> awayTeamID =  {game.awayTeamID} awayTeam Score ={game.awayTeam_Score} </p>             
-          </li>
-        )})}
-      </ul>
+      //     <li key={game.match_id} > 
+      //       <p>Match No: {game.match_id }:</p>
+      //       <p> homeTeamID = {game.homeTeamID} homeTeam Score = {game.homeTeam_Score}</p>
+      //       <p> awayTeamID =  {game.awayTeamID} awayTeam Score ={game.awayTeam_Score} </p>             
+      //     </li>
+      //   )})}
+      // </ul>
+
+      <tbody>
+        <tr>
+          <th>Match ID    </th>
+          <th>Home Team     </th>
+          <th>Home Score </th>
+          <th>Away Team    </th>
+          <th>Away Score   </th>
+
+        </tr>
+        {match.map((game) => (
+  <tr key={game.matchID} >
+          <td>{game.match_id}</td>
+      <td>{game.homeTeamID} </td>
+      <td>   {game.homeTeam_Score}</td>
+      <td>{game.awayTeamID}</td>
+      <td>{game.awayTeam_Score}</td>
+
+  </tr>
+  ))}
+      </tbody>
     );
   }
   
